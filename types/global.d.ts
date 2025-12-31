8ff63fa6c955d50c9f2cb9bdb403a7dc5a63040d
Philip I. Thomas <mail@philipithomas.com>
2025-12-31 12:45:45 -0800

    fix: include one tap config option in types

--- types/global.d.ts
+++ types/global.d.ts
@@ -9,6 +9,7 @@ declare global {
             client_id: string;
             callback: (response: { credential?: string }) => void;
             auto_select?: boolean;
+            cancel_on_tap_outside?: boolean;
           }) => void;
           prompt: () => void;
         };
