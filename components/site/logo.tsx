4607f82d6b3641e9af7776131897b0c3d7406986
Philip I. Thomas <mail@philipithomas.com>
2025-12-31 12:40:33 -0800

    style: enlarge navbar logo

--- components/site/logo.tsx
+++ components/site/logo.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-3 no-underline">
-      <span className="relative h-6 w-28">
+      <span className="relative h-7 w-32">
         <Image
           src="/wordmark.svg"
           alt="Trivet"
